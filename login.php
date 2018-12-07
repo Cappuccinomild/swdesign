ff8775c6fbe96e208893130b16922c234598a423
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2018-12-08 02:00:06 +0900

    커커커밋
    이건 진호

--- login.php
+++ login.php
@@ -6,68 +6,31 @@ require_once './db.php';
 $base = new Layout;
 $base->link = './style.css';
 
-
-
 $db = new DBC;
-
 $db->DBI();
-
-
-
 $id = $_POST['logid'];
-
 $pass = $_POST['logpass'];
-
-
-
 $db->query = "select id, pass, permit,regist_type from member where id='".$id."' and pass=password('".$pass."')";
-
 $db->DBQ();
-
-
-
 $num = $db->result->num_rows;
-
 $data = $db->result->fetch_row();
-
-
-
 $db->DBO();
-
-
-
 if($num==1)
-
 {
-
-	$_SESSION['id'] = $id;
-
-	$_SESSION['permit'] = $data[2];
-
-	$_SESSION['regist_type']=$data[3];
-
-	echo "<script>location.replace('/');</script>";
-
+   $_SESSION['id'] = $id;
+   $_SESSION['permit'] = $data[2];
+   $_SESSION['regist_type']=$data[3];
+   echo "<script>location.replace('/');</script>";
 } else if(($id!="" || $pass!="") && $data[0]!=1)
-
 {
-
-	echo "<script>alert('아이디와 비밀번호가 맞지 않습니다.');</script>";
-
+   echo "<script>alert('아이디와 비밀번호가 맞지 않습니다.');</script>";
 }
 
-
-
 $base->content = "
-<form action='".$_SERVER['PHP_SELF']."' method='post'>
 
+<form action='".$_SERVER['PHP_SELF']."' method='post'>
 	<table style='width: 450px;background-color: #ffffff; margin-left: auto; margin-right: auto; margin-top: 50px; border-radius: 5px; padding: 20px; height: 200px;'>
 
-		<tr>
-
-			<th colspan='2' style='font-family: 휴먼모음T; font-size: 30px; color: #000000;'>로그인</th>
-
-		</tr>
 
 		<tr>
 
@@ -82,19 +45,17 @@ $base->content = "
 		</tr>
 
 		<tr>
-			<td colspan='2'><input type='submit' id='login-btn' value='로그인'/></td>
+			<td colspan='2'><input type='submit' id='login-btn' value='로그인'/><hr /></td>
 		</tr>
 		<tr>
 
-			<td><a href='./registi.php' class='sub-text'>등록</a> <a href='./find.php' class='sub-text'>ID/PASSWORD 찾기</a>
+			<td><a href='./registi.php' class='sub-text'>회원가입</a> <a href='./find.php' class='sub-text'>ID/PASSWORD 찾기</a>
 			</td>
 
 		</tr>
 
 	</table>
-
 </form>
-
 
 ";
 

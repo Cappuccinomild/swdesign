4ba4c4a0d6bdd18c7a129e2e28fffc4c92e49b56
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2018-12-08 15:12:26 +0900

    디자이너페이지 바뀜
    ㄱ

--- designer.php
+++ designer.php
@@ -1,77 +1,61 @@
 <?php
-
 require_once './layout.inc';
 require_once './db.php';
-
 $base = new Layout;
 $base->link = './style.css';
-
-$db = new DBC;
-
-$db->DBI();
-
-$id = $_SESSION['id'];
-
-$db->query = "SELECT title, body, CustomerID, GoodsID FROM feedback WHERE DesignerID = $id";
-
-$db->DBQ();
-
-if($db->result){//값이 존재할 경우
-
-		//메인페이지에 출력한다
-		while($data = $db->result->fetch_row()){//링크를 클릭하면 newbook.html로 이동
-			$base->content .="<a href = './newbook.html'>
-			title : ".$data[0]."<br/>body : ".$data[1]."</a>
-			<br/>-------------------------<br/>";
-		}
-}
-
 
 //상품 등록 페이지
 $base->content .= "<form action='imgsavedemo.php' method='post' enctype='multipart/form-data'>
 
-<fieldset>
+<fieldset style='width:420px; margin-left:auto; margin-right:auto;'>
+<table style='width: 400px;background-color: #ffffff; margin-left: auto; margin-right: auto; margin-top: 50px; border-radius: 5px; padding: 20px; height: 500px;'>
+			<tr>
+				<td><label for='Item' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>상품명</label></td>
+				<td><input type='text' id='Item' name='Item' maxlength='40' size='20' class='designer-text-field'/></td>
+			</tr>
 
-	<p><label for='Item'>Item</label>
-	<input type='text' id='Item' name='Item'
-	maxlength='40' size='7' /></p>
+			<tr>
+				<td><label for='Category' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>카테고리</label></td>
+				<td><input list='cate' id='Category' name='Category' maxlength='40' size='20' autocomplete='off' class='designer-text-field'/></td>
+				<datalist id='cate'>
+				  <option value='Outer'>
+				  <option value='Top'>
+				  <option value='Pants'>
+				</datalist>
 
-	<p><label for='Category'>Category</label>
-	<input list='cate' id='Category' name='Category'
-	maxlength='40' size='7' autocomplete='off'/></p>
+			</tr>
 
-	<datalist id='cate'>
-  <option value='outer'>
-  <option value='top'>
-  <option value='pants'>
-	</datalist>
+			<tr>
+				<td><label for='price' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>가격(원)</label></td>
+				<td><input type='text' id='price' name='price' maxlength='40' size='20' class='designer-text-field'/></td>
+			</tr>
 
-	<p><label for='price'>price</label>
-	<input type='text' id='price' name='price'
-	maxlength='40' size='7' /></p>
+			<tr>
+				<td><label for='color' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>색상</label></td>
+				<td><input type='color' multiple = 'multiple' id='color' name='color' maxlength='40' size='20' /></td>
+			</tr>
 
-	<p><label for='color'>color</label>
-	<input type='color' multiple = 'multiple' id='color' name='color'
-	maxlength='40' size='7' /></p>
+			<tr>
+				<td><label for='size' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>사이즈</label></td>
+				<td><input type='text' id='size' name='size' maxlength='40' size='20' class='designer-text-field'/></td>
+			</tr>
 
-	<p><label for='size'>size</label>
-	<input type='text' id='size' name='size'
-	maxlength='40' size='7' /></p>
+			<tr>
 
-	<p><label for='material'>material</label>
-	<input type='text' id='material' name='material'
-	maxlength='40' size='7' /></p>
+				<td><label for='material' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>재질</label></td>
+				<td><input type='text' id='material' name='material' maxlength='40' size='20' class='designer-text-field'/></td>
+			</tr>
 
-	<p><label for='img'>img</label>
-	<input type='file' multiple='multiple' name='img' id='img'/></p>
+			<tr>
 
+				<td><label for='img' style='font-family: 휴먼모음T; font-size: 20px; color: #545454;'>이미지</label></td>
+				<td><input type='file' multiple='multiple' name='img' id='img'/></td>
+			</tr>
+
+			<tr>
+				<td colspan='2'><input type='submit' value='submit' id='submit-btn'/></td>
+			</tr>
 </fieldset>
-
-<p><input type='submit' value='submit' /></p>
 </form>";
-
-
-$db->DBO();
-
 $base->LayoutMain();
 ?>

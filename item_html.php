456968c48bd0ad8ca902c0bfd43708d7ff0ac2fb
jinhoryu <fbwlsgh93@naver.com>
2018-12-08 19:40:07 +0900

    item_html + css

--- item_html.php
+++ item_html.php
@@ -14,10 +14,15 @@
     $base->link = './style.css';
 
     $base->content="
-    <a href = './item.html'><img src='./images/0.jpg' alt='item_demo' title='item_demo' id='itemimg' width='200px' height='200px' /></a>
-  상품명 : item_demo<br/>색상 : 검정, 흰색, 파랑<br/>사이즈 : S,M,L<br/>디자이너 : 진호<br/>가격 : 3000 원<br/>
-  <hr>
-    <a href = './item.html'><img src='./images/th0.jpg' alt='item_demo' title='item_demo' id='itemimg' width='1000px' height='2500px' /></a>
+    <table class='item-table'>
+      <tr>
+         <td><a href = './item.html'><img src='./images/0.jpg' alt='item_demo' title='item_demo' id='itemimg' width='365px' height='450px' style='margin-right: 100px;'/></a> </td>
+         <td class='item-text'>상품명 : item_demo<br/>색상 : 검정, 흰색, 파랑<br/>사이즈 : S,M,L<br/>디자이너 : 진호<br/>가격 : 3000 원<br/> </td>
+      </tr>
+    </table>
+
+    <hr>
+      <a href = './item.html'><img src='./images/th0.jpg' alt='item_demo' title='item_demo' id='itemimg' width='1000px' height='2500px' /></a>
 
     ";
     $base->LayoutMain();

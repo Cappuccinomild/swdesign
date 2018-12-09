f8bac6f9f42e1a0850e4d22aba1f405596319d2c
Cappuccinomild <33108600+Cappuccinomild@users.noreply.github.com>
2018-12-10 04:26:53 +0900

    리스트출력 수정
    designer_list
    customer

--- customer.php
+++ customer.php
@@ -1,35 +1,90 @@
 <?php
-
+//피드백 진행중인 상품
 require_once './layout.inc';
 require_once './db.php';
 
 $base = new Layout;
 $base->link = './style.css';
 
+$id = $_SESSION['id'];
+if(isset($_GET['page'])){
+	$page=$_GET['page'];
+}
+else {
+	$page=1;
+}
+
 $db = new DBC;
 
 $db->DBI();
 
-
-
-$id = $_SESSION['id'];
-
-$db->query = "SELECT title, body, DesignerID, GoodsID FROM feedback WHERE CustomerID = $id";
+//GoodsID가 0인거는 메세지용
+$db->query = "SELECT COUNT(GoodsID) FROM feedback
+							WHERE CustomerID = '".$id."' AND permit = 0 AND NOT GoodsID = 0 ";
 
 $db->DBQ();
 
 if($db->result){//값이 존재할 경우
+	 $data = $db->result->fetch_row();
+	 $MAX=$data[0];
+}
+else{
+	echo "no";
+}
+$db->DBO();
 
-		//메인페이지에 출력한다
-		while($data = $db->result->fetch_row()){//링크를 클릭하면 newbook.html로 이동
-			$base->content .="<a href = './newbook.html'>
-			title : ".$data[0]."<br/>body : ".$data[1]."</a>
-			<br/>-------------------------<br/>";
-		}
+$firstPageNum = ($page * 16) - 16 ;
+$MAX = ceil($MAX/16) * 16;
+
+$db = new DBC;
+$db1 = new DBC;
+
+$db->DBI();
+$db1->DBI();
+
+$db->query = "SELECT GoodsID FROM feedback WHERE CustomerID = $id AND permit = 0 ORDER BY GoodsID desc LIMIT $firstPageNum, 16 ";
+
+$db->DBQ();
+
+if($db->result){//값이 존재할 경우
+	 $base->content .="<table style='margin-left: auto; margin-right: auto;'>";
+	 //메인페이지에 출력한다
+	 for($i = 0 ; $data = $db->result->fetch_row() ; $i = $i + 1){//링크를 클릭하면 newbook.html로 이동
+		 $db1->query = "SELECT ItemName, thumb FROM item WHERE GoodsID = $data[0] ";
+
+		 $db1->DBQ();
+
+		 $data1 = $db1->result->fetch_row();
+
+		 if($i == 0 || $i == 4 || $i == 8 || $i == 12)
+			 $base->content .="<tr>";
+
+			 $base->content .="<td style='text-align: center; padding: 10px; height: 450px;'> <a href = './item.php?item_id=".$data[0]."'><img src='".$data1[1]."' alt='".$data1[0]."' title='".$data1[0]."' id='itemimg' width='261px' height='341px' /></a><br/>
+			 <a href = './item.php?item_id=".$data[0]."'>".$data1[0]."<br/></a></td>";
+
+			 if($i == 3 || $i == 7 || $i == 11 || $i == 15)
+				 $base->content .= "</tr>";
+
+	 }
+
+	 $base->content .= "</table>";
+
+	 if($page > 1){
+		 $board_num = $page - 1;
+		 $base->content .="<a href = './customer.php?page=".$board_num."'> 이전 ";
+
+	 }
+	 for($board_num = 1 ; $board_num <= $MAX/16 ; $board_num = $board_num+1){
+			 $base->content .="<a href = './customer.php?page=".$board_num."'> ".$board_num. " ";
+	 }
+	 if($page < $MAX/16){
+		 $board_num = $page + 1;
+		 $base->content .="<a href = './customer.php?page=".$board_num."'> 다음 ";
+	 }
 }
 
-
 $db->DBO();
+
 
 $base->LayoutMain();
 ?>
--- designer_list.php
+++ designer_list.php
@@ -7,6 +7,12 @@
   $base->link = './style.css';
 
   $id = $_SESSION['id'];
+  if(isset($_GET['page'])){
+    $page=$_GET['page'];
+  }
+  else {
+    $page=1;
+  }
 
   $db = new DBC;
 
@@ -27,44 +33,44 @@
 
   $db->DBO();
 
-
-  $last = ($_GET['now'] * 10) - 1 ;
-  $now = ($_GET['now'] * 10) - 10 ;
-  $MAX = ceil($MAX/10) * 10;
+  $firstPageNum = ($page * 16) - 16 ;
+  $MAX = ceil($MAX/16) * 16;
 
   //get data
   $db = new DBC;
 
   $db->DBI();
 
-  $db->query = "SELECT GoodsID, CategoryID, ItemName, thumb FROM item WHERE DesignerID = '".$id."' ORDER BY GoodsID desc LIMIT $now, 10 " ;
-                                                                                                    //by idx desc limit 0,5
+  $db->query = "SELECT GoodsID, ItemName, thumb FROM item WHERE DesignerID = '".$id."' ORDER BY GoodsID desc LIMIT $firstPageNum, 16 " ;
   $db->DBQ();
 
   if($db->result){//값이 존재할 경우
         //메인페이지에 출력한다
+        $base->content .="<table style='margin-left: auto; margin-right: auto;'>";
         for($i = 0; $data = $db->result->fetch_row(); $i = $i + 1){//링크를 클릭하면 newbook.html로 이동
+          if($i == 0 || $i == 4 || $i == 8 || $i == 12)
+        		$base->content .="<tr>";
 
-           $base->content .="<td style='margin-left: 20px; margin-right: 20px;'> <a href = './item.php?item_id=".$data[0]."'><img src='".$data[3]."' alt='".$data[2]."' title='".$data[2]."' id='itemimg' width='270px' height='80px' /></a>
-           <a href = './item.php?item_id=".$data[0]."'>상품명 : ".$data[2]."</td></a>";
+           $base->content .="<td style='text-align: center; padding: 10px; height: 450px;'> <a href = './item.php?item_id=".$data[0]."'><img src='".$data[2]."' alt='".$data[1]."' title='".$data[1]."' id='itemimg' width='261px' height='341px' /></a><br/>
+           <a href = './item.php?item_id=".$data[0]."'>상품명 : ".$data[1]."</td></a>";
 
-           if($i == 4)
-             $base->content .= "<br/>";
+           if($i == 3 || $i == 7 || $i == 11 || $i == 15)
+             $base->content .= "</tr>";
         }
 
-        $base->content .= "<br/>";
+        $base->content .= "</table>";
 
-        if($_GET['now'] > 1){
-          $board_num = $_GET['now'] - 1;
-          $base->content .="<a href = './designer_list.php?now=".$board_num."'> 이전 ";
+        if($page > 1){
+          $board_num = $page - 1;
+          $base->content .="<a href = './index.php?page=".$board_num."'> 이전 ";
 
         }
-        for($board_num = 1 ; $board_num <= $MAX/10 ; $board_num = $board_num+1){
-            $base->content .="<a href = './designer_list.php?now=".$board_num."'> ".$board_num. " ";
+        for($board_num = 1 ; $board_num <= $MAX/16 ; $board_num = $board_num+1){
+            $base->content .="<a href = './index.php?page=".$board_num."'> ".$board_num. " ";
         }
-        if($_GET['now'] < $MAX/10){
-          $board_num = $_GET['now'] + 1;
-          $base->content .="<a href = './designer_list.php?now=".$board_num."'> 다음 ";
+        if($page < $MAX/16){
+          $board_num = $page + 1;
+          $base->content .="<a href = './index.php?page=".$board_num."'> 다음 ";
         }
   }
   $base->LayoutMain();

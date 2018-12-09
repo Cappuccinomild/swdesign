b23711cf6ab49ac76aeac87dcc3e3ff719114dd6
jinhoryu <fbwlsgh93@naver.com>
2018-12-09 20:10:33 +0900

    ㅇㅇ

--- index.php
+++ index.php
@@ -43,16 +43,19 @@
 
   if($db->result){//값이 존재할 경우
         //메인페이지에 출력한다
+  		$base->content .="<table>";
         for($i = 0; $data = $db->result->fetch_row(); $i = $i + 1){//링크를 클릭하면 newbook.html로 이동
 
+        	if($i % 5 == 0)
+        		$base->content .="<tr>";
            $base->content .="<td style='margin-left: 20px; margin-right: 20px;'> <a href = './item.php?item_id=".$data[0]."'><img src='".$data[3]."' alt='".$data[2]."' title='".$data[2]."' id='itemimg' width='270px' height='80px' /></a>
            <a href = './item.php?item_id=".$data[0]."'>상품명 : ".$data[2]."</td></a>";
 
-           if($i == 4)
-             $base->content .= "<br/>";
+           if($i % 5 == 4)
+             $base->content .= "</tr>";
         }
 
-        $base->content .= "<br/>";
+        $base->content .= "</table>";
 
         if($_GET['now'] > 1){
           $board_num = $_GET['now'] - 1;

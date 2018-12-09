ca496cced66103630613b5f6cb36863b98b80029
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2018-12-10 06:21:16 +0900

    잠

--- item.php
+++ item.php
@@ -20,12 +20,21 @@ $db->DBQ();
 if($db->result){//값이 존재할 경우
 		//메인페이지에 출력한다
 		$data = $db->result->fetch_row();
-
+		$order='./order.php';
 			$base->content .="
 			<table class='item-table'>
 				<td><img src='".$data[7]."' alt='".$data[0]."' title='".$data[0]."' id='itemimg' width='365px' height='450px' style='margin-right: 50px; float:left;'/></td>
-	        	<td class='item-text' width='450px'><h2>".$data[0]."</h2><hr/><br/><p>색상 : 검정, 흰색, 파랑</p><p>사이즈 : ".$data[3]."</p><p>디자이너 : ".$data[5]."</p><p style='color: #bc3831;'>가격 : ".$data[1]." 원</p> </td>
+	      <td class='item-text' width='450px'>
+				<h2>".$data[0]."</h2>
+				<hr/><br/>
+				<p>색상 : 검정, 흰색, 파랑</p>
+				<p>사이즈 : ".$data[3]."</p>
+				<p>디자이너 : ".$data[5]."</p>
+				<p style='color: #bc3831;'>가격 : ".$data[1]." 원</p>
+				<a href='./order.php?item_id=".$item_id."' class='buy_but' style='text-align: center; height: 35px; width:180px; font-size:28px; padding : 10px; margin-top:20px; margin-bottom:20px;display:inline-block;'>구매하기</a>
+				</td>
 	        </table>
+					<br/><hr class='style-eight'/>
 
 	     <img src='".$data[6]."' alt='".$data[0]."' title='".$data[0]."' id='itemimg' width='1000px'/>";
 
--- registi.php
+++ registi.php
@@ -24,7 +24,7 @@ $base->content = "
             <td><label for='name' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>가입 유형</label></td>
 
             <td class='checkbox-registi' style='margin-top:10px;'>
-               <input type='radio' name='regist_type' value='customer' /> 고객
+               <input type='radio' name='regist_type' value='customer' selected='selected'/> 고객
                  <input type='radio' name='regist_type' value='designer' /> 디자이너
             </td>
 
@@ -34,7 +34,7 @@ $base->content = "
 
             <td><label for='name' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>이름</label></td>
 
-            <td><input type='text' size='20' name='name' class='text-field' placeholder='이름''/></td>
+            <td><input type='text' size='20' name='name' class='text-field' placeholder='이름' required/></td>
 
          </tr>
 
@@ -42,7 +42,7 @@ $base->content = "
 
             <td><label for='id' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>아이디</label></td>
 
-            <td><input type='text' size='16' name='id' class='text-field'  placeholder='아이디'/></td>
+            <td><input type='text' size='16' name='id' class='text-field'  placeholder='아이디' required/></td>
 
          </tr>
 
@@ -50,7 +50,7 @@ $base->content = "
 
             <td><label for='pass1' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>비밀번호</label></td>
 
-            <td><input type='password' size='16' name='pass1' class='text-field'  placeholder='비밀번호'/></td>
+            <td><input type='password' size='16' name='pass1' class='text-field'  placeholder='비밀번호' required/></td>
 
          </tr>
 
@@ -58,7 +58,7 @@ $base->content = "
 
             <td><label for='pass2' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>비밀번호 확인</label></td>
 
-            <td><input type='password' size='16' name='pass2' class='text-field'  placeholder='비밀번호 확인'/></td>
+            <td><input type='password' size='16' name='pass2' class='text-field'  placeholder='비밀번호 확인' required/></td>
 
          </tr>
 
@@ -66,7 +66,7 @@ $base->content = "
 
             <td><label for='mail' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>이메일</label></td>
 
-            <td><input type='text' size='16' name='mail' class='text-field'  placeholder='example@email.com'/></td>
+            <td><input type='text' size='16' name='mail' class='text-field' pattern = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' title = 'example@email.com 형식으로 입력해주세요' placeholder='example@email.com' required/></td>
 
          </tr>
 
@@ -74,7 +74,7 @@ $base->content = "
 
             <td><label for='phone' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>휴대전화</label></td>
 
-            <td><input type='text' size='20' name='phone' class='text-field' placeholder='휴대전화'/></td>
+            <td><input type='text' size='20' name='phone' class='text-field' pattern = '[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}' title = '012-3456-7891 형식으로 입력해주세요' placeholder='휴대전화' required/></td>
 
          </tr>
 
@@ -82,7 +82,7 @@ $base->content = "
 
             <td><label for='address' style='font-family: 휴먼모음T; font-size: 20px; color: #000000; float: left;'>주소</label></td>
 
-            <td><input type='text' size='30' name='address' class='text-field' placeholder='주소''/></td>
+            <td><input type='text' size='30' name='address' class='text-field' placeholder='주소' required/></td>
 
          </tr>
 

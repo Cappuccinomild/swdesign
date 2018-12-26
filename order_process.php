cf0ffd8bb72badde182a15a60aac3b029cfe3cf4
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2018-12-26 19:32:36 +0900

    order_process 주석
    실적 +1

--- order_process.php
+++ order_process.php
@@ -16,6 +16,7 @@ $base = new Layout;
 
 $base->link = './style.css';
 
+//주문페이지에서 넘어온 값들을 모두 변수에 저장
 $item_id=$_GET['item_id'];
 
 $designer=$_GET['designer'];
@@ -36,17 +37,17 @@ $mail = $_POST['mail'];
 
 $memo = $_POST['memo'];
 
-$body="색상: $color\n사이즈: $size\n요구사항: $memo";
+$body="색상: $color\n사이즈: $size\n요구사항: $memo"; //메모와 함께 전달 될 내용
 
-$allowed_ext = array('jpg','jpeg','png','gif');
+$allowed_ext = array('jpg','jpeg','png','gif'); //허가된 이미지파일 확장자
 
-if(isset($_POST['img'])){
+if(isset($_POST['img'])){ //이미지가 있을 때만 if 내부 실행
   echo '1';
   $error = $_FILES['img']['error'];
   $name = $_FILES['img']['name'];
   $ext = array_pop(explode('.', $name));
 
-  if( $error != UPLOAD_ERR_OK ) {
+  if( $error != UPLOAD_ERR_OK ) { //이미지 에러 발생 시
    switch( $error ) {
   	 case UPLOAD_ERR_INI_SIZE:
   	 case UPLOAD_ERR_FORM_SIZE:
@@ -61,22 +62,23 @@ if(isset($_POST['img'])){
    }
    exit;
   }
-  if( !in_array($ext, $allowed_ext) ) {
+  if( !in_array($ext, $allowed_ext) ) { //허가된 이미지파일 확장자에 첨부파일의 확장자가 없을 경우
    echo "<script>alert('허용되지 않는 이미지 확장자입니다.');history.back();</script>";
    exit;
   }
 
-   $imglink = "./images/".$item_id."_".$id.".".$ext;
+   $imglink = "./images/".$item_id."_".$id.".".$ext; //연결된 링크 저장
 
-  move_uploaded_file( $_FILES['img']['tmp_name'], $imglink);
+  move_uploaded_file( $_FILES['img']['tmp_name'], $imglink); //해당 위치에 업로드
 }
 
+//피드백에 주문 정보 저장
 $db->query = "INSERT INTO feedback VALUES('".$id."', '".$designer."','".$item_id."' , 0, '".$body."', '".$imglink."' ,0)";
 
 $db->DBQ();
 
 
-if(!$db->result)
+if(!$db->result) //쿼리가 제대로 실행되지 않았을 경우
 
 {
 
@@ -93,7 +95,7 @@ if(!$db->result)
 } else
 
 {
-	$db->query="UPDATE item SET total_order = total_order+1 WHERE GoodsID=".$item_id;
+	$db->query="UPDATE item SET total_order = total_order+1 WHERE GoodsID=".$item_id; //해당 상품의 주문량 1 증가
 	$db->DBQ();
 
 	echo "<script>alert('주문이 완료 되었습니다.');location.replace('/');</script>";

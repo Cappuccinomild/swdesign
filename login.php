b52287bac9cc23e05e99765775c5f8dafa4ff7c0
meaning2415 <33250675+meaning2415@users.noreply.github.com>
2018-12-26 19:26:06 +0900

    login 주석 +1
    실적 +1

--- login.php
+++ login.php
@@ -8,20 +8,27 @@ $base->link = './style.css';
 
 $db = new DBC;
 $db->DBI();
+
+//id와 password값을 post 형태로 받음
 $id = $_POST['logid'];
 $pass = $_POST['logpass'];
+
+//id와 password에 해당하는 정보 쿼리로 받음
 $db->query = "select id, pass, permit,regist_type from member where id='".$id."' and pass=password('".$pass."')";
 $db->DBQ();
+
 $num = $db->result->num_rows;
 $data = $db->result->fetch_row();
 $db->DBO();
+
+//해당하는 정보가 1개면 접속을 하고 세션값으로 나머지 정보를 받으며 메인페이지로 돌아감
 if($num==1)
 {
    $_SESSION['id'] = $id;
    $_SESSION['permit'] = $data[2];
    $_SESSION['regist_type']=$data[3];
    echo "<script>location.replace('/');</script>";
-} else if(($id!="" || $pass!="") && $data[0]!=1)
+} else if(($id!="" || $pass!="") && $data[0]!=1) //아이디 비밀번호 정보가 맞지않을 경우
 {
    echo "<script>alert('아이디와 비밀번호가 맞지 않습니다.');</script>";
 }
@@ -30,22 +37,16 @@ $base->content = "
 
 <form action='".$_SERVER['PHP_SELF']."' method='post'>
    <table style='width: 450px;background-color: #ffffff; margin-left: auto; margin-right: auto; margin-top: 50px; border-radius: 5px; padding: 20px; height: 200px;'>
-
-
       <tr>
          <td class='text-login'> 로그인 </td>
       </tr>
 
       <tr>
-
          <td><input type='text' name='logid' size='20' class='text-field' style='padding: 10px; width: 94%;' placeholder='아이디'/></td>
-
       </tr>
 
       <tr>
-
          <td><input type='password' name='logpass' size='20' class='text-field'  style='padding: 10px; width: 94%;' placeholder='비밀번호'/></td>
-
       </tr>
 
       <tr>
